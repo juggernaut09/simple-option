c245c612fe6810acf378d8838a2b8d4456a44508
venkattejaRaavi <venkatteja.ravi@gmail.com>
2020-10-14 21:32:40 +0530

    wrote core logic code

--- src/contract.rs
+++ src/contract.rs
@@ -1,23 +1,29 @@
 use cosmwasm_std::{
-    to_binary, Api, Binary, Env, Extern, HandleResponse, InitResponse, MessageInfo, Querier,
-    StdResult, Storage,
+    to_binary, Api, BankMsg, Binary, Context, Env, Extern, HandleResponse, HumanAddr, InitResponse,
+    MessageInfo, Querier, StdError, StdResult, Storage,
 };
 
-use crate::error::ContractError;
-use crate::msg::{CountResponse, HandleMsg, InitMsg, QueryMsg};
+use crate::msg::{ConfigResponse, HandleMsg, InitMsg, QueryMsg};
 use crate::state::{config, config_read, State};
 
 // Note, you can use StdResult in some functions where you do not
 // make use of the custom errors
 pub fn init<S: Storage, A: Api, Q: Querier>(
     deps: &mut Extern<S, A, Q>,
-    _env: Env,
+    env: Env,
     info: MessageInfo,
     msg: InitMsg,
 ) -> StdResult<InitResponse> {
+    if msg.expires <= env.block.height {
+        return Err(StdError::generic_err("Cannot create expired option"));
+    }
+
     let state = State {
-        count: msg.count,
-        owner: deps.api.canonical_address(&info.sender)?,
+        creator: info.sender.clone(),
+        owner: info.sender.clone(),
+        collateral: info.sent_funds,
+        counter_offer: msg.counter_offer,
+        expires: msg.expires,
     };
     config(&mut deps.storage).save(&state)?;
 
@@ -27,41 +33,105 @@ pub fn init<S: Storage, A: Api, Q: Querier>(
 // And declare a custom Error variant for the ones where you will want to make use of it
 pub fn handle<S: Storage, A: Api, Q: Querier>(
     deps: &mut Extern<S, A, Q>,
-    _env: Env,
     info: MessageInfo,
+    env: Env,
     msg: HandleMsg,
-) -> Result<HandleResponse, ContractError> {
+) -> StdResult<HandleResponse> {
     match msg {
-        HandleMsg::Increment {} => try_increment(deps),
-        HandleMsg::Reset { count } => try_reset(deps, info, count),
+        HandleMsg::Transfer { recipient } => handle_transfer(deps, info, recipient),
+        HandleMsg::Execute {} => handle_execute(deps, info, env),
+        HandleMsg::Burn {} => handle_burn(deps, info, env),
     }
 }
-
-pub fn try_increment<S: Storage, A: Api, Q: Querier>(
-    deps: &mut Extern<S, A, Q>,
-) -> Result<HandleResponse, ContractError> {
-    config(&mut deps.storage).update(|mut state| -> Result<_, ContractError> {
-        state.count += 1;
-        Ok(state)
-    })?;
-
-    Ok(HandleResponse::default())
-}
-
-pub fn try_reset<S: Storage, A: Api, Q: Querier>(
+pub fn handle_transfer<S: Storage, A: Api, Q: Querier>(
     deps: &mut Extern<S, A, Q>,
     info: MessageInfo,
-    count: i32,
-) -> Result<HandleResponse, ContractError> {
-    let api = &deps.api;
-    config(&mut deps.storage).update(|mut state| -> Result<_, ContractError> {
-        if api.canonical_address(&info.sender)? != state.owner {
-            return Err(ContractError::Unauthorized {});
-        }
-        state.count = count;
-        Ok(state)
-    })?;
-    Ok(HandleResponse::default())
+    recipient: HumanAddr,
+) -> StdResult<HandleResponse> {
+    let mut state: State = config(&mut deps.storage).load()?;
+
+    // ensure msg.sender is the owner
+    if info.sender != state.owner {
+        return Err(StdError::generic_err("sender is Unauthorized"));
+    }
+
+    // set ne owner on state
+    state.owner = recipient.clone();
+    config(&mut deps.storage).save(&state)?;
+
+    let res = Context::new();
+    Ok(res.into())
+}
+
+pub fn handle_execute<S: Storage, A: Api, Q: Querier>(
+    deps: &mut Extern<S, A, Q>,
+    info: MessageInfo,
+    env: Env,
+) -> StdResult<HandleResponse> {
+    // ensure message sender is the owner
+    let state: State = config(&mut deps.storage).load()?;
+    if info.sender != state.owner {
+        return Err(StdError::generic_err("sender is Unauthorized"));
+    }
+
+    // ensure not expired
+    if env.block.height >= state.expires {
+        return Err(StdError::generic_err("option expired"));
+    }
+    // ensure sending proper counter_offer
+    if info.sent_funds != state.counter_offer {
+        return Err(StdError::generic_err(format!(
+            "must send exact counter_offer: {:?}",
+            state.counter_offer
+        )));
+    }
+    // release counter_offer to creator
+    let mut res = Context::new();
+    res.add_message(BankMsg::Send {
+        from_address: env.contract.address.clone(),
+        to_address: state.creator,
+        amount: state.counter_offer,
+    });
+
+    // release collateral to sender
+    res.add_message(BankMsg::Send {
+        from_address: env.contract.address,
+        to_address: state.owner,
+        amount: state.collateral,
+    });
+
+    // delete the option
+    config(&mut deps.storage).remove();
+    Ok(res.into())
+}
+
+pub fn handle_burn<S: Storage, A: Api, Q: Querier>(
+    deps: &mut Extern<S, A, Q>,
+    info: MessageInfo,
+    env: Env,
+) -> StdResult<HandleResponse> {
+    let state: State = config(&mut deps.storage).load()?;
+    // ensure is expired
+    if env.block.height < state.expires {
+        return Err(StdError::generic_err("option not yet expired"));
+    }
+
+    // ensure not sending the counter_offer
+    if !info.sent_funds.is_empty() {
+        return Err(StdError::generic_err("don't send funds with burn"));
+    }
+
+    // release collateral to creator
+    let mut res = Context::new();
+    res.add_message(BankMsg::Send {
+        from_address: env.contract.address,
+        to_address: state.creator,
+        amount: state.collateral,
+    });
+
+    // delete the option
+    config(&mut deps.storage).remove();
+    Ok(res.into())
 }
 
 pub fn query<S: Storage, A: Api, Q: Querier>(
@@ -70,82 +140,84 @@ pub fn query<S: Storage, A: Api, Q: Querier>(
     msg: QueryMsg,
 ) -> StdResult<Binary> {
     match msg {
-        QueryMsg::GetCount {} => to_binary(&query_count(deps)?),
+        QueryMsg::Config {} => to_binary(&query_config(deps)?),
     }
 }
 
-fn query_count<S: Storage, A: Api, Q: Querier>(deps: &Extern<S, A, Q>) -> StdResult<CountResponse> {
+fn query_config<S: Storage, A: Api, Q: Querier>(
+    deps: &Extern<S, A, Q>,
+) -> StdResult<ConfigResponse> {
     let state = config_read(&deps.storage).load()?;
-    Ok(CountResponse { count: state.count })
+    Ok(state)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
-    use cosmwasm_std::{coins, from_binary};
-
-    #[test]
-    fn proper_initialization() {
-        let mut deps = mock_dependencies(&[]);
-
-        let msg = InitMsg { count: 17 };
-        let info = mock_info("creator", &coins(1000, "earth"));
-
-        // we can just call .unwrap() to assert this was a success
-        let res = init(&mut deps, mock_env(), info, msg).unwrap();
-        assert_eq!(0, res.messages.len());
-
-        // it worked, let's query the state
-        let res = query(&deps, mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: CountResponse = from_binary(&res).unwrap();
-        assert_eq!(17, value.count);
-    }
-
-    #[test]
-    fn increment() {
-        let mut deps = mock_dependencies(&coins(2, "token"));
-
-        let msg = InitMsg { count: 17 };
-        let info = mock_info("creator", &coins(2, "token"));
-        let _res = init(&mut deps, mock_env(), info, msg).unwrap();
-
-        // beneficiary can release it
-        let info = mock_info("anyone", &coins(2, "token"));
-        let msg = HandleMsg::Increment {};
-        let _res = handle(&mut deps, mock_env(), info, msg).unwrap();
-
-        // should increase counter by 1
-        let res = query(&deps, mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: CountResponse = from_binary(&res).unwrap();
-        assert_eq!(18, value.count);
-    }
-
-    #[test]
-    fn reset() {
-        let mut deps = mock_dependencies(&coins(2, "token"));
-
-        let msg = InitMsg { count: 17 };
-        let info = mock_info("creator", &coins(2, "token"));
-        let _res = init(&mut deps, mock_env(), info, msg).unwrap();
-
-        // beneficiary can release it
-        let unauth_info = mock_info("anyone", &coins(2, "token"));
-        let msg = HandleMsg::Reset { count: 5 };
-        let res = handle(&mut deps, mock_env(), unauth_info, msg);
-        match res {
-            Err(ContractError::Unauthorized {}) => {}
-            _ => panic!("Must return unauthorized error"),
-        }
-
-        // only the original creator can reset the counter
-        let auth_info = mock_info("creator", &coins(2, "token"));
-        let msg = HandleMsg::Reset { count: 5 };
-        let _res = handle(&mut deps, mock_env(), auth_info, msg).unwrap();
-
-        // should now be 5
-        let res = query(&deps, mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: CountResponse = from_binary(&res).unwrap();
-        assert_eq!(5, value.count);
-    }
-}
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
+//     use cosmwasm_std::{coins, from_binary};
+//
+//     #[test]
+//     fn proper_initialization() {
+//         let mut deps = mock_dependencies(&[]);
+//
+//         let msg = InitMsg { count: 17 };
+//         let info = mock_info("creator", &coins(1000, "earth"));
+//
+//         // we can just call .unwrap() to assert this was a success
+//         let res = init(&mut deps, mock_env(), info, msg).unwrap();
+//         assert_eq!(0, res.messages.len());
+//
+//         // it worked, let's query the state
+//         let res = query(&deps, mock_env(), QueryMsg::GetCount {}).unwrap();
+//         let value: CountResponse = from_binary(&res).unwrap();
+//         assert_eq!(17, value.count);
+//     }
+//
+//     #[test]
+//     fn increment() {
+//         let mut deps = mock_dependencies(&coins(2, "token"));
+//
+//         let msg = InitMsg { count: 17 };
+//         let info = mock_info("creator", &coins(2, "token"));
+//         let _res = init(&mut deps, mock_env(), info, msg).unwrap();
+//
+//         // beneficiary can release it
+//         let info = mock_info("anyone", &coins(2, "token"));
+//         let msg = HandleMsg::Increment {};
+//         let _res = handle(&mut deps, mock_env(), info, msg).unwrap();
+//
+//         // should increase counter by 1
+//         let res = query(&deps, mock_env(), QueryMsg::GetCount {}).unwrap();
+//         let value: CountResponse = from_binary(&res).unwrap();
+//         assert_eq!(18, value.count);
+//     }
+//
+//     #[test]
+//     fn reset() {
+//         let mut deps = mock_dependencies(&coins(2, "token"));
+//
+//         let msg = InitMsg { count: 17 };
+//         let info = mock_info("creator", &coins(2, "token"));
+//         let _res = init(&mut deps, mock_env(), info, msg).unwrap();
+//
+//         // beneficiary can release it
+//         let unauth_info = mock_info("anyone", &coins(2, "token"));
+//         let msg = HandleMsg::Reset { count: 5 };
+//         let res = handle(&mut deps, mock_env(), unauth_info, msg);
+//         match res {
+//             Err(ContractError::Unauthorized {}) => {}
+//             _ => panic!("Must return unauthorized error"),
+//         }
+//
+//         // only the original creator can reset the counter
+//         let auth_info = mock_info("creator", &coins(2, "token"));
+//         let msg = HandleMsg::Reset { count: 5 };
+//         let _res = handle(&mut deps, mock_env(), auth_info, msg).unwrap();
+//
+//         // should now be 5
+//         let res = query(&deps, mock_env(), QueryMsg::GetCount {}).unwrap();
+//         let value: CountResponse = from_binary(&res).unwrap();
+//         assert_eq!(5, value.count);
+//     }
+// }
